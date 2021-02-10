41b18547b8374374c5eaac04415b0217b96aa512
olgagnatenko13 <76459058+olgagnatenko13@users.noreply.github.com>
2021-02-10 04:40:39 +0000

    feat: lesson2 project files

--- train.py
+++ train.py
@@ -14,13 +14,17 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
+path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+ds = TabularDatasetFactory.from_delimited_files(path)
 
 x, y = clean_data(ds)
 
 # TODO: Split data into train and test sets.
 
-### YOUR CODE HERE ###a
+test_size = 0.25
+random_state = 7
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = test_size, random_state = random_state)
 
 run = Run.get_context()
 
